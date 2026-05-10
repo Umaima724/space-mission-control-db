5193f7f9adaa1e6e201df48731fe0f53024efda1
Umaima724 <umaimamubashar19@gmail.com>
2026-05-10 16:42:12 +0500

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove('token')
+      Cookies.remove('user')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
